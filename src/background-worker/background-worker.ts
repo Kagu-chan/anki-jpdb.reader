c36dec38bf9416fb72b50e2ebcfe16e79720a958
Kagu-chan <k.boese@kagu-chan.de>
2025-01-19 17:51:42 +0100

    migrate install and context menus

--- src/background-worker/background-worker.ts
+++ src/background-worker/background-worker.ts
@@ -1,12 +1,20 @@
+import {
+  addContextMenu,
+  addInstallListener,
+  OnInstalledReason,
+  openOptionsPage,
+} from '@shared/extension';
+import { ParsePageCommand, ParseSelectionCommand } from '@shared/messages';
 import { GradeCardCommandHandler } from './jpdb-card-actions/grade-card-command.handler';
 import { RunDeckActionCommandHandler } from './jpdb-card-actions/run-deck-action-command.handler';
 import { UpdateCardStateCommandHandler } from './jpdb-card-actions/update-card-state-command.handler';
 import { BackgroundCommandHandlerCollection } from './lib/background-command-handler-collection';
-import { installInstallListeners } from './lib/install-install-listeners';
-import { installParseInitiator } from './lib/install-parse-initiator';
 import { installParser } from './lib/parser/install-parser';
 import { LookupController } from './lookup/lookup-controller';
 import { LookupTextCommandHandler } from './lookup/lookup-text-command.handler';
+
+const parsePageCommand = new ParsePageCommand();
+const parseSelectionCommand = new ParseSelectionCommand();
 
 const lookupController = new LookupController();
 const lookupTextCommandHandler = new LookupTextCommandHandler(lookupController);
@@ -22,9 +30,33 @@ const handlerCollection = new BackgroundCommandHandlerCollection(
   runDeckActionCommandHandler,
 );
 
-installParseInitiator();
 installParser();
 
-installInstallListeners();
-
 handlerCollection.listen();
+
+addInstallListener(async ({ reason }) => {
+  if (reason === OnInstalledReason.INSTALL) {
+    await openOptionsPage();
+  }
+
+  // TODO: OnUpdate Open a new tab with the release notes
+  // NOTE: OnUpdate In the future we may use this for schema updates
+});
+
+addContextMenu(
+  {
+    id: 'parse-page',
+    title: 'Parse Page',
+    contexts: ['page'],
+  },
+  (_, { id }) => parsePageCommand.send(id!),
+);
+
+addContextMenu(
+  {
+    id: 'parse-selection',
+    title: 'Parse Selection',
+    contexts: ['selection'],
+  },
+  (_, { id }) => parseSelectionCommand.send(id!),
+);
