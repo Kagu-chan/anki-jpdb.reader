3bca75416c1d020aa50f13a64b91abff3c7bdea3
chinokusari <k.boese@kagu-chan.de>
2025-08-03 17:14:06 +0200

    fix(text-highlighter): Improved CSS injection and reduced flickering.

--- changelog/0.6.4.ts
+++ changelog/0.6.4.ts
@@ -3,26 +3,32 @@ import { Category, ChangelogEntry } from './types';
 export const _064: ChangelogEntry[] = [
   {
     type: 'add',
-    description: 'Added an im- and export of settings',
+    description: 'Added import and export of settings.',
     category: Category.Settings,
     issue: 66,
   },
   {
     type: 'add',
-    description: 'Added a link to the GitHub issues page',
+    description: 'Added a link to the GitHub issues page.',
     category: Category.Settings,
     issue: 'N/A',
   },
   {
     type: 'add',
-    description: 'Added a debug mode for host configuration',
+    description: 'Added a debug mode for host configuration.',
     category: Category.Settings,
     issue: 'N/A',
   },
   {
     type: 'fix',
-    description: 'Fixed a bug where the parser would not parse nested elements',
+    description: 'Fixed a bug where the parser would not parse nested elements.',
     category: Category.Parser,
     issue: 238,
+  },
+  {
+    type: 'fix',
+    description: 'Improved CSS injection and reduced flickering.',
+    category: Category.Texthighlighter,
+    issue: [235, 272],
   },
 ];
--- src/shared/extension/inject-style.ts
+++ src/shared/extension/inject-style.ts
@@ -1,45 +1,85 @@
-const installedStyles = new Map<string, { file: string; raw?: string }>();
+const tabs = new Map<number, { file?: string; raw?: string }>();
 
 chrome.tabs.onRemoved.addListener((tabId) => {
-  installedStyles.forEach((config, key) => {
-    if (key.startsWith(`${tabId}-`)) {
-      installedStyles.delete(key);
-    }
-  });
+  tabs.delete(tabId);
+});
+chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
+  if (changeInfo.status === 'loading') {
+    tabs.delete(tabId);
+  }
 });
 
-export const injectStyle = async (tabId: number, file: string, raw?: string): Promise<void> => {
-  const key = `${tabId}-${file}`;
-
-  if (installedStyles.has(key)) {
-    const config = installedStyles.get(key);
-    const remove = (
-      cfg:
-        | Pick<chrome.scripting.CSSInjection, 'files'>
-        | Pick<chrome.scripting.CSSInjection, 'css'>,
-    ): Promise<void> =>
-      chrome.scripting.removeCSS({ target: { tabId, allFrames: true }, ...cfg }).catch(() => {
-        /* noop */
-      });
-
-    await remove({ files: [config!.file] });
-
-    if (config?.raw?.length) {
-      await remove({ css: config.raw });
-    }
-  }
-
-  await chrome.scripting.insertCSS({
+const getPath = (file: string): string => `css/${file}.css`;
+const insert = (
+  tabId: number,
+  cfg:
+    | {
+        css: string;
+        files?: undefined;
+      }
+    | {
+        css?: undefined;
+        files: string[];
+      },
+): Promise<void> =>
+  chrome.scripting.insertCSS({
     target: { tabId, allFrames: true },
-    files: [`css/${file}.css`],
+    ...cfg,
   });
 
-  if (raw?.length) {
-    await chrome.scripting.insertCSS({
+const remove = (
+  tabId: number,
+  cfg:
+    | {
+        css: string;
+        files?: undefined;
+      }
+    | {
+        css?: undefined;
+        files: string[];
+      },
+): Promise<void> =>
+  chrome.scripting
+    .removeCSS({
       target: { tabId, allFrames: true },
-      css: raw,
+      ...cfg,
+    })
+    .catch(() => {
+      /* noop */
     });
+
+export const injectStyle = async (tabId: number, file?: string, raw?: string): Promise<void> => {
+  const currentConfig = tabs.get(tabId) || {};
+  const filePath = file?.length ? getPath(file) : undefined;
+  const awaits: Promise<void>[] = [];
+
+  // If nothing changed, ignore the request
+  if (currentConfig.raw === raw && currentConfig.file === filePath) {
+    return;
   }
 
-  installedStyles.set(key, { file, raw });
+  // If the file changed, update the tabs file configuration
+  if (currentConfig.file !== filePath) {
+    const remProm = currentConfig.file
+      ? remove(tabId, { files: [currentConfig.file] })
+      : Promise.resolve();
+    const addProm = filePath ? remProm.then(() => insert(tabId, { files: [filePath] })) : remProm;
+
+    awaits.push(addProm);
+  }
+
+  // If the css changed, update the tabs raw configuration
+  if (currentConfig.raw !== raw) {
+    const remProm = currentConfig.raw?.length
+      ? remove(tabId, { css: currentConfig.raw })
+      : Promise.resolve();
+    const addProm = raw?.length ? remProm.then(() => insert(tabId, { css: raw })) : remProm;
+
+    awaits.push(addProm);
+  }
+
+  await Promise.all(awaits);
+
+  // Update the tabs configuration
+  tabs.set(tabId, { file: filePath, raw });
 };
